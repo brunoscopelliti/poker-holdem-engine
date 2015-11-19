5098e36502835805cbf156eb6dfde374c78cae08
Bruno Scopelliti <bru.scopelliti@gmail.com>
2015-11-19 07:37:01 +0100

    getNextPlayerIndex

---
+++ tests/get-nextplayer-index.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+const sut = require('../lib/get-nextplayer-index');
+
+const tape = require('tape');
+const tcase = require('tape-case');
+const chalk = require('chalk');
+const sinon = require('sinon');
+
+
+const progressive = Symbol.for('hand-progressive');
+
+tape('get-nextplayer-index', t => t.end());
+
+tcase([
+  { description: 'get index of the next player', args: [ 0, 5 ], result: 1 },
+  { args: [ 1, 5 ], result: 2 },
+  { args: [ 4, 5 ], result: 0 },
+  { args: [ 4, 6 ], result: 5 },
+  { args: [ 5, 6 ], result: 0 },
+], function(current, total) {
+
+  return sut(current, total);
+
+});
