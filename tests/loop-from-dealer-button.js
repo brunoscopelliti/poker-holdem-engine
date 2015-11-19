98760d3b9cdd8e95764a5b56c78dbfddd9d5afc4
Bruno Scopelliti <bru.scopelliti@gmail.com>
2015-11-19 07:46:30 +0100

    hasDB as symbol

--- tests/loop-from-dealer-button.js
+++ tests/loop-from-dealer-button.js
@@ -16,10 +16,12 @@ tape('loop on players starting from Dealer Button', function(t){
   const players = [
     { name: 'john', chips: 2400 },
     { name: 'mark', chips: 1250 },
-    { name: 'paul', chips: 3250, hasDB: true },
+    { name: 'paul', chips: 3250 },
     { name: 'mike', chips: 500 },
     { name: 'luis', chips: 1500 },
   ];
+
+  players[2][Symbol.for('hasDB')] = true;
 
   sut(players, function(player) {
 
@@ -33,7 +35,7 @@ tape('loop on players starting from Dealer Button', function(t){
   }).then(function() {
     t.equal(players.filter(player => typeof player.count != 'undefined').length, players.length, 'loop over all the players');
     t.ok(players[2].count === 1 && players[1].count === players.length, 'check the loop order');
-    t.equal(Object.getOwnPropertySymbols(players[2]).length, 0, '[passed] symbol is deleted');
+    t.equal(Object.getOwnPropertySymbols(players[2]).length, 1, '[passed] symbol is deleted');
     t.end()
   })
 
