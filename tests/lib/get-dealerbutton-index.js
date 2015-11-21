ea8c2f307556ad1b26d66507f4b02cc936bd531c
Bruno Scopelliti <bru.scopelliti@gmail.com>
2015-11-21 07:57:26 +0100

    test db index

---
+++ tests/lib/get-dealerbutton-index.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+const sut = require('../../lib/get-dealerbutton-index');
+
+const tape = require('tape');
+const tcase = require('tape-case');
+const chalk = require('chalk');
+const sinon = require('sinon');
+
+
+tape('get-dealerbutton-index', t => t.end());
+
+tape('check dealer button index is computed properly', function(t){
+
+  let hasDB = Symbol.for('hasDB');
+  let players = [{[hasDB]: false}, {[hasDB]: true}, {}, {}];
+
+  t.equal(sut(players), 1, 'check db index');
+
+  t.end();
+
+});
