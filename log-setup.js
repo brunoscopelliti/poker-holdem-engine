1d9201f771b002448f7b998a014b75a2f55a42d7
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-01-13 07:49:50 +0100

    reformat code

--- log-setup.js
+++ log-setup.js
@@ -2,30 +2,30 @@
 'use strict';
 
 const config = require('./config');
-
 const winston = module.exports = require('winston');
 
-const gamestorySettings = {
-  console: {
-    level: 'info',
-    colorize: true,
-    label: 'gamestory'
-  }
-};
+if (winston && winston.loggers){
 
-if (config.SAVE_LOG){
-  gamestorySettings.file = { filename: './log-gamestory.txt' };
+  // log game history
+
+  const gamestorySettings = { console: { level: 'info', colorize: true, label: 'gamestory' } };
+  if (config.SAVE_LOG){
+    gamestorySettings.file = { filename: './log-gamestory.txt' };
+  }
+  winston.loggers.add('gamestory', gamestorySettings);
+
+
+  // log errors
+
+  winston.loggers.add('errors', {
+    console: {
+      level: 'error',
+      colorize: true,
+      label: 'errors'
+    },
+    file: {
+      filename: './log-errors.txt'
+    }
+  });
+
 }
-
-winston.loggers.add('gamestory', gamestorySettings);
-
-winston.loggers.add('errors', {
-  console: {
-    level: 'error',
-    colorize: true,
-    label: 'errors'
-  },
-  file: {
-    filename: './log-errors.txt'
-  }
-});
