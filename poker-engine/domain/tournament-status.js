151012f1cf9044d4e8fe214618ae60b03506d35e
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-04-17 07:20:38 +0200

    renamed game-status into tournament-status

--- poker-engine/domain/tournament-status.js
+++ poker-engine/domain/tournament-status.js
@@ -1,21 +1,21 @@
 
 'use strict';
 
-const status = Object.create(null);
+const tournamentStatus = Object.create(null);
 
 // tournament is active,
 // and the game proceed
-status.play = 'play';
+tournamentStatus.play = 'play';
 
 // tournament is active,
 // but the game is temporarily paused
-status.pause = 'pause';
+tournamentStatus.pause = 'pause';
 
 // tournament is active, and the game proceed
 // but the current game will be the last of the tournament
-status.latest = 'latest';
+tournamentStatus.latest = 'latest';
 
 // tournament has finished
-status.stop = 'stop';
+tournamentStatus.stop = 'stop';
 
-exports = module.exports = Object.freeze(status);
+exports = module.exports = Object.freeze(tournamentStatus);
