d36c1de10142006e3f514b6258ffe85d6f2f95d9
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-04-25 07:41:18 +0200

    wrap promise.all

--- demo.js
+++ demo.js
@@ -26,19 +26,27 @@ engine.on('gamestate:updated', function(data, done) {
 const tournamentID = config.tournamentId;
 const players = config.players;
 
-Promise.all(players.map(function(player){
-  return new Promise(function(res, rej) {
-    const port = player.serviceUrl.match(/:([\d]{4})\/$/)[1];
-    const child = exec('node ./index.js', { cwd: `./demo-players/${player.name}/`, env: { PORT: port } }, function(err, stdout, stderr) {
-      if (err) {
-        console.log(chalk.bold.red('An error occurred while trying to open child process'), err);
-        return rej(err);
-      }
-      res();
-    });
-    child.stdout.on('data', data => console.log(chalk.bold.green(`${player.name}'s stdout:`), data));
-    child.stderr.on('data', data => console.log(chalk.bold.red(`${player.name}'s stderr:`), data));
-  });
-})).then(function() {
+startPlayerServices().then(function() {
   engine.start(tournamentID, players);
 });
+
+
+
+
+
+function startPlayerServices() {
+  return Promise.all(players.map(function(player){
+    return new Promise(function(res, rej) {
+      const port = player.serviceUrl.match(/:([\d]{4})\/$/)[1];
+      const child = exec('node ./index.js', { cwd: `./demo-players/${player.name}/`, env: { PORT: port } }, function(err, stdout, stderr) {
+        if (err) {
+          console.log(chalk.bold.red('An error occurred while trying to open child process'), err);
+          return rej(err);
+        }
+        res();
+      });
+      child.stdout.on('data', data => console.log(chalk.bold.green(`${player.name}'s stdout:`), data));
+      child.stderr.on('data', data => console.log(chalk.bold.red(`${player.name}'s stderr:`), data));
+    });
+  }));
+}
