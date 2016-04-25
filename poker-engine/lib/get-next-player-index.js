433f62001b3ec3e0c76d9ed754558a54ff035e2f
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-04-25 08:59:44 +0200

    get-next-player-index

---
+++ poker-engine/lib/get-next-player-index.js
@@ -0,0 +1,25 @@
+
+'use strict';
+
+/**
+ * @function
+ * @name getNextPlayerIndex
+ * @desc find the index of the player next to the one, whose index is passed as input
+ *
+ * @param {Array} players:
+ *  list of the player who play the current tournament;
+ * @param {Number} playerIndex
+ *
+ * @returns {Number} index of the next player
+ */
+exports = module.exports = function getNextPlayerIndex(players, playerIndex){
+
+  const next = playerIndex + 1;
+
+  if (playerIndex >= players.length-1){
+    return next % players.length;
+  }
+
+  return next;
+
+}
---
+++ tests/poker-engine/lib/get-next-player-index.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+const tape = require('tape');
+const tcase = require('tape-case');
+const sinon = require('sinon');
+
+const sut = require('../../../poker-engine/lib/get-next-player-index');
+
+
+
+const progressive = Symbol.for('hand-progressive');
+
+tape('get-nextplayer-index', t => t.end());
+
+tcase([
+  { description: 'get index of the next player', args: [ 0 ], result: 1 },
+  { args: [ 1 ], result: 2 },
+  { args: [ 2 ], result: 3 },
+  { args: [ 3 ], result: 0 },
+  { args: [ 4 ], result: 1 },
+  { args: [ 5 ], result: 2 }
+], function(playerIndex) {
+  const players = [{ name: 'arale' }, { name: 'bender' }, { name: 'marvin' }, { name: 'wall-e' }]
+  return sut(players, playerIndex);
+});
