e3640ed375e1ecdeec0ab1794ef9325c10d710b6
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-04-30 07:23:55 +0200

    player's bet/pay methods draft

--- poker-engine/domain-utils/player-factory.js
+++ poker-engine/domain-utils/player-factory.js
@@ -3,6 +3,9 @@
 
 const config = require('../../config');
 
+const playerStatus = require('../domain/player-status');
+
+
 const logger = require('../../storage/logger');
 
 
@@ -12,19 +15,126 @@ const logger = require('../../storage/logger');
 // const sortByRank = require('poker-rank');
 // const getCombinations = require('poker-combinations');
 
-const status = require('../domain/player-status');
+
 
 const save = require('../../storage/storage').save;
 
 
 
 // const hasDB = Symbol.for('has-dealer-button');
-// const gameProgressive = Symbol.for('game-progressive');
-// const roundProgressive = Symbol.for('hand-progressive');
 
+
+const bet_ = Symbol('internal-bet-method');
 
 const actions = {
-  bet() {}
+
+  [bet_](gs, amount) {
+
+    if (amount > this.chips){
+      amount = this.chips;
+    }
+
+
+    if (amount == this.chips) {
+      this[Symbol.for('is-all-in')] = true;
+    }
+    else {
+
+      // the total amount of chips which the player
+      // has bet in the current hand
+      const chipsBet = this.chipsBet + amount
+
+      if (chipsBet < gs.callAmount){
+
+        // // player is betting less than the required amount;
+        // // since he is not betting all the chips he owns
+        // // we treat this as a "fold" declaration
+        // // gamestory.info('%s (%d) folded', this.name, this.id, { id: gs.handId, type: 'status' });
+        // return this.fold(gs);
+
+      }
+      else if (chipsBet > gs.callAmount) {
+        // // player is betting a raise;
+        // // since the player is not going allin,
+        // // we accept only raise of an amount that is multiple of the small blind.
+        // const raiseAmount = safeDiff(chipsBet, gs.callAmount);
+        // if (raiseAmount % gs.sb != 0){
+        //   // in case the raise amount is not a multiple of the small blind
+        //   // the raise, is treated as a simple call.
+        //   amount = safeDiff(amount, raiseAmount);
+        // }
+      }
+
+    }
+
+
+
+    // update chip values
+    // for the player, and on the gamestate
+    this.chipsBet += amount;
+    this.chips -= amount;
+    gs.pot += amount;
+    gs.callAmount = Math.max(this.chipsBet, gs.callAmount);
+
+    // gamestory.info('Game state after %s (%d)\'s bet: %s', this.name, this.id, JSON.stringify({ pot:gs.pot, callAmount: gs.callAmount, player: { name: this.name, chips: this.chips, chipsBet: this.chipsBet } }), { id: gs.handId, type: 'bet' });
+
+    // return save(gs, { type: 'bet', handId: gs.handId, session: gs.session, playerId: this.id, amount: amount });
+
+
+
+
+  },
+
+
+  /**
+   * @function
+   * @name bet
+   * @desc TODO
+   *
+   * @param {Object} gs:
+   *  the gamestate object
+   * @param {Number}
+   *  the amount of chips the player has to pay
+   *
+   * @returns {Promise} the promise is resolved when bet data is stored
+   */
+  bet(gs, betAmount) {
+
+    // since bet is called with the amount returned by
+    // the player's web service,
+    // it's important to sanitize the amount
+
+    const amount = sanitizeAmount(betAmount);
+
+
+    // when the amount is safe ... TODO
+
+    this[bet_](gs, amount);
+
+    // TODO
+    // save data & return a promise!
+
+  },
+
+
+  /**
+   * @function
+   * @name pay
+   * @desc
+   *  Update gamestate, and player's chips.
+   *  It's used when the player has not the possibility to define the amount of his bet.
+   *
+   * @param {Object} gs:
+   *  the gamestate object
+   * @param {Number}
+   *  the amount of chips the player has to pay
+   *
+   * @returns {void}
+   */
+  pay(gs, amount) {
+    this[bet_](gs, amount);
+  }
+
 };
 
 
@@ -34,22 +144,40 @@ const actions = {
  * @name isValidPlayer
  * @desc check if the input parameter match the "player interface".
  *
- * @param {object} player
+ * @param {Object} player
  *
- * @returns {bool} true when the input parameter is a valid "player" object; false otherwise
+ * @returns {Boolean} true when the input parameter is a valid "player" object; false otherwise
  */
 function isValidPlayer(player){
   return player.id && player.name && player.serviceUrl;
 }
 
+/**
+ * @private
+ * @function
+ * @name sanitizeAmount
+ * @desc check if the value is a valid bet amount
+ *
+ * @param {Number} amount
+ *
+ * @returns {Number} amount (valid)
+ */
+function sanitizeAmount(amount){
 
+  if (typeof amount != 'number'){
+    amount = Number.parseInt(amount, 10);
+  }
+
+  return amount > 0 ? amount : 0;
+
+}
 
 /**
  * @function
  * @name factory
  * @desc create a new "player" object
  *
- * @param {object} obj
+ * @param {Object} obj
  *  - player.id
  *  - player.name
  *  - player.serviceUrl
@@ -74,7 +202,7 @@ exports = module.exports = function factory(obj){
   player.version = 'Poker folder star!';
 
   // status of the player
-  player.status = status.active;
+  player.status = playerStatus.active;
 
   // amount of chips available
   player.chips = config.BUYIN;
--- tests/poker-engine/domain-utils/player-factory.js
+++ tests/poker-engine/domain-utils/player-factory.js
@@ -1,8 +1,10 @@
 
 'use strict';
 
-const sinon = require('sinon');
 const tape = require('tape');
+const sinon = require('sinon');
+
+const getSymbol = require('../../test-utils/get-symbol');
 
 // const request = require('request');
 // const postStub = sinon.stub(request, 'post');
@@ -38,6 +40,81 @@ tape('create new player', function(t) {
   t.ok(Array.isArray(player.cards), 'player cards');
 
   t.equal(typeof player.bet, 'function', 'player can bet');
+  t.equal(typeof player.pay, 'function', 'player can pay');
 
   t.end();
+});
+
+
+
+
+tape('player pay', t => t.end());
+
+tape('player pay call internal bet method', function(t) {
+
+  const gamestate = {};
+  const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
+
+  const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-bet-method'));
+
+  player.pay(gamestate, 50);
+
+  t.ok(betStub.calledOnce);
+  t.ok(betStub.calledWith(gamestate, 50));
+
+  t.end();
+
+});
+
+
+
+
+tape('player bet', t => t.end());
+
+tape('the amount is sanitized (negative number), than the internal bet method is called', function(t) {
+
+  const gamestate = {};
+  const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
+
+  const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-bet-method'));
+
+  player.bet(gamestate, -50);
+
+  t.ok(betStub.calledOnce);
+  t.ok(betStub.calledWith(gamestate, 0));
+
+  t.end();
+
+});
+
+tape('the amount is sanitized (NaN), than the internal bet method is called', function(t) {
+
+  const gamestate = {};
+  const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
+
+  const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-bet-method'));
+
+  player.bet(gamestate, 'hello');
+
+  t.ok(betStub.calledOnce);
+  t.ok(betStub.calledWith(gamestate, 0));
+
+  t.end();
+
+});
+
+tape('the amount is sanitized (valid amount), than the internal bet method is called', function(t) {
+
+  const gamestate = {};
+  const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
+
+  const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-bet-method'));
+
+  player.bet(gamestate, '50');
+
+  t.ok(betStub.calledOnce);
+  t.ok(betStub.calledWith(gamestate, 50));
+
+  t.end();
+
 });
