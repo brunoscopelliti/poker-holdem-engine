39244e6e6959e0f6965ab9e717e13c908f62430f
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-05-01 07:30:50 +0200

    pay blinds

---
+++ poker-engine/domain-utils/pay-blinds.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+const getDealerButtonIndex = require('../lib/get-dealer-button-index');
+const getNextActivePlayerIndex = require('../lib/get-next-active-player-index');
+
+
+
+/**
+ * @function
+ * @name payBlinds
+ * @desc make the players next to the dealer button pay small/big blind amount
+ *
+ * @param {Object} gs:
+ *  the gamestate object
+ *
+ * @returns {void}
+ */
+exports = module.exports = function payBlinds(gs){
+
+  const hasBB = Symbol.for('has-big-blind');
+
+  const dealerButtonIndex = getDealerButtonIndex(gs.players);
+
+  // index of the player
+  // who has to pay the small blind
+  const smallBlindIndex = getNextActivePlayerIndex(gs.players, dealerButtonIndex);
+
+  gs.players[smallBlindIndex].pay(gs, gs.sb);
+
+
+  // index of the player
+  // who has to pay the big blind
+  const bigBlindIndex = getNextActivePlayerIndex(gs.players, smallBlindIndex);
+
+  gs.players[bigBlindIndex].pay(gs, 2*gs.sb);
+  gs.players[bigBlindIndex][hasBB] = true;
+
+};
---
+++ tests/poker-engine/domain-utils/pay-blinds.js
@@ -0,0 +1,85 @@
+
+'use strict';
+
+const tape = require('tape');
+const sinon = require('sinon');
+
+const config = require('../../../config');
+
+const playerStatus = require('../../../poker-engine/domain/player-status');
+
+const sut = require('../../../poker-engine/domain-utils/pay-blinds');
+
+
+const hasDB = Symbol.for('has-dealer-button');
+
+
+tape('pay-blinds', t => t.end());
+
+tape('players next the dealer button pay blinds', function(t) {
+
+  const paySpy = sinon.spy();
+
+  const arale = { name: 'arale', status: playerStatus.active };
+  const bender = { name: 'bender', status: playerStatus.active };
+  const marvin = { name: 'marvin', status: playerStatus.active, pay: paySpy };
+  const walle = { name: 'wall-e', status: playerStatus.active, pay: paySpy };
+
+  const gamestate = {
+    sb: 20,
+    players: [arale, bender, marvin, walle]
+  };
+
+  gamestate.players[1][hasDB] = true;
+
+  sut(gamestate);
+
+  t.ok(paySpy.calledTwice);
+
+  const firstCall = paySpy.getCall(0);
+
+  t.ok(firstCall.calledWith(gamestate, 20));
+  t.ok(firstCall.calledOn(marvin));
+
+  const secondCall = paySpy.getCall(1);
+
+  t.ok(secondCall.calledWith(gamestate, 40));
+  t.ok(secondCall.calledOn(walle));
+
+  t.end();
+
+});
+
+tape('players next the dealer button pay blinds (check only active players)', function(t) {
+
+  const paySpy = sinon.spy();
+
+  const arale = { name: 'arale', status: playerStatus.active, pay: paySpy };
+  const bender = { name: 'bender', status: playerStatus.active };
+  const marvin = { name: 'marvin', status: playerStatus.active, pay: paySpy };
+  const walle = { name: 'wall-e', status: playerStatus.out };
+
+  const gamestate = {
+    sb: 25,
+    players: [arale, bender, marvin, walle]
+  };
+
+  gamestate.players[1][hasDB] = true;
+
+  sut(gamestate);
+
+  t.ok(paySpy.calledTwice);
+
+  const firstCall = paySpy.getCall(0);
+
+  t.ok(firstCall.calledWith(gamestate, 25));
+  t.ok(firstCall.calledOn(marvin));
+
+  const secondCall = paySpy.getCall(1);
+
+  t.ok(secondCall.calledWith(gamestate, 50));
+  t.ok(secondCall.calledOn(arale));
+
+  t.end();
+
+});
