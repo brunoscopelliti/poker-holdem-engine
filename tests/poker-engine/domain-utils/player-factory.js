e518575ab0d383f4c049eb10e3f9d5addc8fe244
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-05-09 05:36:29 +0200

    unit test player#fold

--- tests/poker-engine/domain-utils/player-factory.js
+++ tests/poker-engine/domain-utils/player-factory.js
@@ -4,13 +4,16 @@
 const tape = require('tape');
 const sinon = require('sinon');
 
-const getSymbol = require('../../test-utils/get-symbol');
+const request = require('request');
+const postStub = sinon.stub(request, 'post');
 
-// const request = require('request');
-// const postStub = sinon.stub(request, 'post');
+const engine = require('../../../index');
+
 
 const config = require('../../../config');
 const playerStatus = require('../../../poker-engine/domain/player-status');
+
+const getSymbol = require('../../test-utils/get-symbol');
 
 const sut = require('../../../poker-engine/domain-utils/player-factory');
 
@@ -34,7 +37,7 @@ tape('create new player', function(t) {
   t.strictEqual(player.name, 'arale');
   t.strictEqual(player.serviceUrl, 'http:arale.com');
 
-  t.strictEqual(player.status, playerStatus.active, 'player is active');
+  t.strictEqual(player.status, 'active', 'player is active');
   t.strictEqual(player.chips, config.BUYIN, 'player has chips');
   t.strictEqual(player.chipsBet, 0, 'player hasnt bet anything yet')
   t.ok(Array.isArray(player.cards), 'player cards');
@@ -48,19 +51,19 @@ tape('create new player', function(t) {
 
 
 
-tape('player pay', t => t.end());
+tape('player#pay', t => t.end());
 
 tape('player pay call internal bet method', function(t) {
 
   const gamestate = {};
   const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
 
-  const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-update-method'));
+  const updateStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-update-method'));
 
   player.pay(gamestate, 50);
 
-  t.ok(betStub.calledOnce);
-  t.ok(betStub.calledWith(gamestate, 50));
+  t.ok(updateStub.calledOnce);
+  t.ok(updateStub.calledWith(gamestate, 50));
 
   t.end();
 
@@ -69,52 +72,22 @@ tape('player pay call internal bet method', function(t) {
 
 
 
-tape('player bet', t => t.end());
+tape('player#fold', t => t.end());
 
-// tape('the amount is sanitized (negative number), than the internal bet method is called', function(t) {
-//
-//   const gamestate = {};
-//   const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
-//
-//   const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-update-method'));
-//
-//   player.bet(gamestate, -50);
-//
-//   t.ok(betStub.calledOnce);
-//   t.ok(betStub.calledWith(gamestate, 0));
-//
-//   t.end();
-//
-// });
-//
-// tape('the amount is sanitized (NaN), than the internal bet method is called', function(t) {
-//
-//   const gamestate = {};
-//   const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
-//
-//   const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-update-method'));
-//
-//   player.bet(gamestate, 'hello');
-//
-//   t.ok(betStub.calledOnce);
-//   t.ok(betStub.calledWith(gamestate, 0));
-//
-//   t.end();
-//
-// });
-//
-// tape('the amount is sanitized (valid amount), than the internal bet method is called', function(t) {
-//
-//   const gamestate = {};
-//   const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
-//
-//   const betStub = sinon.stub(player, getSymbol(Object.getPrototypeOf(player), 'internal-update-method'));
-//
-//   player.bet(gamestate, '50');
-//
-//   t.ok(betStub.calledOnce);
-//   t.ok(betStub.calledWith(gamestate, 50));
-//
-//   t.end();
-//
-// });
+tape('update player status to "folded"', function(t) {
+
+  const gamestate = {};
+  const player = sut({ name: 'arale', id: 'a1', serviceUrl: 'http:arale.com' });
+
+  engine.once('gamestate:updated', function(data, done){
+    t.equal(player.status, 'folded');
+    t.equal(data.type, 'status');
+    t.equal(data.playerId, 'a1');
+    t.equal(data.status, 'folded');
+    done();
+    t.end();
+  });
+
+  player.fold(gamestate);
+
+});
